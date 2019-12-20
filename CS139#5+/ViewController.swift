04e92f1ee10d49b79da366acd23dbe60583db7f0
Genrikh302 <genrikh.mayorov@gmail.com>
2019-12-20 15:07:03 +0300

    Initial Commit

---
+++ CS139#5+/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CS139#5+
+//
+//  Created by Genrikh Mayorov on 20/12/2019.
+//  Copyright © 2019 Genrikh Mayorov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
